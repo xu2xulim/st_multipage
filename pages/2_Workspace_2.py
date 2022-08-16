7e699605f791bb9a2fa3b4c53e3635dd28ee3075
Sunny Lim <limhss@gmail.com>
2022-08-16 19:25:52 +0800

    S

--- pages/2_Workspace_2.py
+++ pages/2_Workspace_2.py
@@ -5,3 +5,17 @@ else:
     with st.sidebar :
         st.write("Session State :")
         st.json(st.session_state)
+
+import asyncio
+from telethon import TelegramClient, events
+
+# These example values won't work. You must get your own api_id and
+# api_hash from https://my.telegram.org, under API Development.
+api_id = 1379563
+api_hash = '2b01df6cde6a17328b8d34bc5680188b'
+
+async def main():
+  client = TelegramClient('session_name', api_id, api_hash)
+  await client.start()
+
+asyncio.run(main())
