7ebfa53572a97089f408993fdf0dccced30043c4
Sunny Lim <limhss@gmail.com>
2022-08-16 19:53:31 +0800

    S

--- pages/2_Workspace_2.py
+++ pages/2_Workspace_2.py
@@ -17,5 +17,6 @@ api_hash = '2b01df6cde6a17328b8d34bc5680188b'
 async def main():
   client = TelegramClient('session_name', api_id, api_hash)
   await client.start()
+  st.write(client.get_me)
 
 asyncio.run(main())
