bee273a9a67ae36018f0ef6f2ec33aa4178bcd35
Sunny Lim <limhss@gmail.com>
2022-08-17 17:01:19 +0800

    S

--- pages/2_Workspace_2.py
+++ pages/2_Workspace_2.py
@@ -1,11 +1,13 @@
 import streamlit as st
+import requests
+
 if not st.session_state['authentication_status']  :
     st.stop()
 else:
     with st.sidebar :
         st.write("Session State :")
         st.json(st.session_state)
-import requests
+
 
 url = "https://dialpad.com/api/v2/contacts?apikey=c47h8ZcHRVWABfjr4MukxEMY2Qg25QQfDhFtgKYbkr2aANwwvBKqjVQngCVB9CuNwN3A8PYbvhkkL4UT8g3FZ6XXfR23FDMuTyXF"
 
