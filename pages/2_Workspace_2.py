e701851af4b272da6534422a8521f2a7fce9a265
Sunny Lim <limhss@gmail.com>
2022-08-17 17:05:18 +0800

    S

--- pages/2_Workspace_2.py
+++ pages/2_Workspace_2.py
@@ -15,4 +15,4 @@ headers = {"Accept": "application/json"}
 
 response = requests.get(url, headers=headers)
 
-st.dataframe(response.json()['items'])
+st.dataframe(response.json()['items']).fillna{""}
