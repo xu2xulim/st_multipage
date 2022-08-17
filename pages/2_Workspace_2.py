47592a7ca01ab528d710c433e163d94073c78b50
Sunny Lim <limhss@gmail.com>
2022-08-17 17:02:51 +0800

    S

--- pages/2_Workspace_2.py
+++ pages/2_Workspace_2.py
@@ -8,7 +8,7 @@ else:
         st.write("Session State :")
         st.json(st.session_state)
 
-
+st.write("Harrison Thornhill")
 url = "https://dialpad.com/api/v2/contacts?apikey=c47h8ZcHRVWABfjr4MukxEMY2Qg25QQfDhFtgKYbkr2aANwwvBKqjVQngCVB9CuNwN3A8PYbvhkkL4UT8g3FZ6XXfR23FDMuTyXF"
 
 headers = {"Accept": "application/json"}
