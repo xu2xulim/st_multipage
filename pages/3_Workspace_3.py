ffbe28380455aabae6bb30181897a9608290e9a5
Sunny Lim <limhss@gmail.com>
2022-08-18 16:50:10 +0800

    S

--- pages/3_Workspace_3.py
+++ pages/3_Workspace_3.py
@@ -1,5 +1,6 @@
 import streamlit as st
-
+import requests
+import pandas as pd
 
 if 'authentication_status' not in st.session_state.keys():
     st.session_state['authentication_status'] = False
@@ -10,3 +11,15 @@ else:
     with st.sidebar :
         st.write("Session State :")
         st.json(st.session_state)
+
+st.write("Harrison Thornhill")
+url = "https://getpocket.com/v3/oauth/request"
+
+headers = {"Accept": "application/json"}
+payload = {
+    "consumer_key":"103406-fe5e558e83e7075fb5c1f39",
+    "redirect_uri":"milynnus:authorizationFinished"
+    }
+response = requests.get(url, json=payload, headers=headers)
+
+st.dataframe(response.json())
