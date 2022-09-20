38974c9364f74f2ace45431796b7f7f7311fd0e6
Sunny Lim <limhss@gmail.com>
2022-09-20 17:37:31 +0800

    S

--- pages/1_Workspace_1.py
+++ pages/1_Workspace_1.py
@@ -1,45 +1,35 @@
 import streamlit as st
-from io import StringIO
-from trello import TrelloClient
-import pandas as pd
+#from io import StringIO
+#from trello import TrelloClient
+#import pandas as pd
 import os
 from deta import Deta
+
 
 if not st.session_state['authentication_status']  :
     st.stop()
 else:
+    db = Deta(os.environ.get('DEV_PROJECT_ID')).Base('deta_pm_base')
+    col1, col2, col3 = st.columns(3)
+
+    with col1:
+       st.metric("Tasks (Total)", db.fetch().count)
+
+    with col2:
+       st.metric("Completed", db.fetch({"Status" : "Completed"}).count)
+
+    with col3:
+       st.metric("Overdue", db.fetch({"Status" : "Overdue"}).count)
+
+
+
+
     with st.sidebar :
         st.write("Session State :")
         st.json(st.session_state)
 
-st.warning ("Upload a file like  a CSV to create a list of dictionaries")
-uploaded_file = st.file_uploader("Choose a file")
-if uploaded_file is not None:
-     # To read file as bytes:
-     bytes_data = uploaded_file.getvalue()
-     #st.write(bytes_data)
 
-     # To convert to a string based IO:
-     stringio = StringIO(uploaded_file.getvalue().decode("utf-8"))
-     #st.write(stringio)
 
-     # To read file as string:
-     string_data = stringio.read()
-     #st.write(string_data)
 
-     # Can be used wherever a "file-like" object is accepted:
-     dataframe = pd.read_csv(uploaded_file).fillna("")
-     st.write(dataframe)
-     # Easy way to get a dictitionary from a CSV
-     dd = dataframe.to_dict("records")
-
-     db = Deta(os.environ.get('DEV_PROJECT_ID')).Base('deta_pm_base')
-
-     for item in dd :
-         #st.write(type(item), item)
-         db.put(item)
-
-st.metric("Tasks (Total)", db.fetch().count)
-st.metric("Tasks (Completed)", db.fetch({"Status" : "Completed"}).count)
 #client = TrelloClient(api_key = os.environ.get('TRELLO_API_KEY'), token = os.environ.get('TRELLO_TOKEN'))
 #client.list_boards()
