643c1580e1b6fde58c011e8965e5eeff4c34a589
Sunny Lim <limhss@gmail.com>
2022-09-20 18:22:16 +0800

    S

--- pages/3_Upcoming Deadlines.py
+++ pages/3_Upcoming Deadlines.py
@@ -8,34 +8,13 @@ if 'authentication_status' not in st.session_state.keys():
 if not st.session_state['authentication_status']  :
     st.stop()
 else:
+    st.title("Show Eduardo Tasks Instead")
+    db = Deta(os.environ.get('DEV_PROJECT_ID')).Base('deta_pm_base')
+    res = db.fetch({"Status?ne" : "Completed", "Resources?contains" : "Eduardo Vargas"})
+    df = pd.DataFrame(res.items)
+    df_select = df[["Task", "Start Date", "Priority", "Resources"]].sort_values(by=['Priority'])
+    st.table(df_select)
     with st.sidebar :
+
         st.write("Session State : (For Debug Only)")
         #st.json(st.session_state)
-
-st.write("Harrison Thornhill")
-url = "https://getpocket.com/v3/oauth/request"
-
-headers = {"Content-Type": "application/json",
-    }
-payload = {
-    "consumer_key" : "103406-fe5e558e83e7075fb5c1f39",
-    "redirect_uri" : "milynnus:authorizationFinished"
-    }
-response = requests.post(url, json=payload, headers=headers)
-
-st.write(response.text)
-code = response.text.split("=")[-1]
-st.write (code)
-
-url = "https://getpocket.com/v3/oauth/authorize"
-headers = {
-    "Content-Type": "application/json"
-    }
-
-data = {
-    "consumer_key" : "103406-fe5e558e83e7075fb5c1f39",
-    "code" : code
-}
-
-response = requests.post(url, json=data, headers=headers)
-st.write(response.text)
